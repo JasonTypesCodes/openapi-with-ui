a94eb6f4b11746988d916652bb859ac8cca7f887
Jason Schindler <schindlerj@objectcomputing.com>
2021-03-12 12:24:43 -0600

    Removes security

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,14 +2,11 @@ package com.example;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.security.annotation.Secured;
-import io.micronaut.security.rules.SecurityRule;
 
 @Controller("/hello")
 public class HelloController {
 
 	@Get
-	@Secured(SecurityRule.IS_ANONYMOUS)
 	public String sayHello() {
 		return "Hello World!";
 	}
